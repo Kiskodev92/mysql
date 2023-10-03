aa5ce390c693addc1a9ab5b4a7b684dc9cda619c
Kiskotkd <fjgp92@hotmail.com>
2023-10-03 11:11:42 +0200

    dia2

--- Data.js
+++ Data.js
@@ -18,46 +18,63 @@ async function main()
     //                                    "calle VARCHAR(200), "+
     //                                    "numero INT, " +
     //                                    "ciudad VARCHAR(60))";
+    //let [result] = await connection.query(sql);
     //     console.log("Tabla creada");
 
 
     // let sql = "INSERT INTO alumnos (idalumno, apellido1, apellido2, id_cursoalum)"+
-    //           "VALUES (6,\"Mendez\", \"Galvan\", 1)";
-    // let sql = "INSERT INTO alumnos (idalumno, apellido1, apellido2, id_cursoalum)"+
-    //            "VALUES (7,\"Repiso\", \"Ortega\", 2)";
-    // let sql = "INSERT INTO alumnos (idalumno, apellido1, apellido2, id_cursoalum)"+
+    //           "VALUES (6,\"Mendez\", \"Galvan\", 1),"+
+    //            "VALUES (7,\"Repiso\", \"Ortega\", 2),"+
     //            "VALUES (8,\"Borja\", \"Ferrari\", 1)";
+    //let [result] = await connection.query(sql);
         //console.log("Alumno creado");
 
 
     // let sql = "INSERT INTO notas (idnotas, id_estudiante, id_asignatura, fecha, Nota)"+
-    //            "VALUES (3, 3, 1, \"2003-03-03\", 3)";
-    // let sql = "INSERT INTO notas (idnotas, id_estudiante, id_asignatura, fecha, Nota)"+
-    //            "VALUES (5, 6, 1, \"2009-11-02\", 6)";
-    // let sql = "INSERT INTO notas (idnotas, id_estudiante, id_asignatura, fecha, Nota)"+
+    //            "VALUES (3, 3, 1, \"2003-03-03\", 3),"+
+    //            "VALUES (5, 6, 1, \"2009-11-02\", 6),"+
     //            "VALUES (6, 2, 1, \"2019-12-02\", 10)";
+    //let [result] = await connection.query(sql);
         // console.log("Nota creada");
 
 
     // let sql = "INSERT INTO profesores (idprofe, nombre, apellido1)"+
-    //           "VALUES (6, \"Antonio\", \"Flores\")";
-    // let sql = "INSERT INTO profesores (idprofe, nombre, apellido1)"+
-    //           "VALUES (7, \"Tamara\", \"Lago\")";
-    // let sql = "INSERT INTO profesores (idprofe, nombre, apellido1)"+
+    //           "VALUES (6, \"Antonio\", \"Flores\"),"+
+    //           "VALUES (7, \"Tamara\", \"Lago\"),"+
     //           "VALUES (8, \"Pedro\", \"Feijo\")";
+    //let [result] = await connection.query(sql);
         // console.log("Profe creado");
 
     // let sql = "DROP TABLE direccion"
+    //let [result] = await connection.query(sql);
     //     console.log("tabla borrada");
 
     // let sql = "UPDATE notas set Nota = 0"
+    //let [result] = await connection.query(sql);
         // console.log("nota actualizada")
 
     // let sql = "SELECT apellido1, apellido2 FROM alumnos";
     // let sql = "SELECT * FROM profesores";
     // let sql = "DELETE FROM notas WHERE fecha < DATE_SUB(NOW(), INTERVAL 10 YEAR)"
     // let sql = "UPDATE notas SET Nota = 5 WHERE Nota < 5"
-        console.log("Datos obtenidos")
+    //let [result] = await connection.query(sql);
+        // console.log("Datos obtenidos")
+
+    //RETOS DIA 2
+    // let sql = "SELECT AVG(Nota) FROM notas"
+    // console.log("media actualizada")
+    // let sql = "SELECT COUNT (*) alumnos"
+    // console.log("conteo total");
+    // let sql = "SELECT Nota FROM notas WHERE Nota > 5 AND fecha >= 2019-01-01"
+    // console.log("Dato actualizado")
+    // let sql = "SELECT * FROM alumnos WHERE Start_Bootcamp = 2023"
+    // console.log("Dato actualizado")
+    // let sql = "SELECT idsubjets, COUNT (*) AS numteacher FROM subprof GROUP BY idsubjets"
+    // console.log("profes por asignatura");
+    // let sql = "SELECT id_estudiante, Nota FROM notas WHERE (id_estudiante BETWEEN 1 AND 20) OR (Nota > 8 AND YEAR(fecha) = YEAR(CURDATE())-1);"
+    // console.log("Id y nota");
+    // let sql = "SELECT id_estudiante, AVG(nota) AS media_arit FROM notas WHERE YEAR(fecha) = YEAR(CURDATE()) - 1 GROUP BY id_estudiante;"
+    // console.log("Media aritmetica");
     let [result] = await connection.query(sql);
 
     // console.log(result);
